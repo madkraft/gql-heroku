5bc3084f3d9cb826508551574d00bf279d0b6ca8
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2019-02-02 13:10:39 +0100

    add lib

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import { https } from "firebase-functions";
-import { configureServer } from "./server";
+// import { https } from "firebase-functions";
+// import { configureServer } from "./server";
 
-const server = configureServer();
-const api = https.onRequest(server);
-module.exports = { api };
+// const server = configureServer();
+// const api = https.onRequest(server);
+// module.exports = { api };
+
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
