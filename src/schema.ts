9f24b360137df3b136be3eff636dd052f73a5955
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2019-02-02 13:52:41 +0100

    check

--- src/schema.ts
+++ src/schema.ts
@@ -2,13 +2,96 @@ import { gql } from "apollo-server";
 
 export const typeDefs = gql`
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    tweets: [String]!
+    user(id: String!): String!
   }
 `;
 
 export const resolvers = {
   Query: {
-    hello: () => "world"
+    tweets() {
+      return ["hello tweet"];
+    },
+    user(_: undefined, args: { id: string }) {
+      return `USERID is ${args.id}`;
+    }
   }
 };
+
+// export const typeDefs = gql`
+//   type User {
+//     id: ID!
+//     screenName: String!
+//     statusesCount: Int!
+//     tweets: [Tweets]!
+//   }
+
+//   type Tweets {
+//     id: ID!
+//     text: String!
+//     userId: String!
+//     user: User!
+//     likes: Int!
+//   }
+
+//   type Query {
+//     tweets: [Tweets]
+//     user(id: String!): User
+//   }
+// `;
+
+// export const resolvers = {
+//   User: {
+//     async tweets(user: User) {
+//       try {
+//         const userTweets = await admin
+//           .firestore()
+//           .collection("tweets")
+//           .where("userID", "==", user.id)
+//           .get();
+
+//         return userTweets.docs.map((tweet) => tweet.data());
+//       } catch (error) {
+//         throw new ApolloError(error);
+//       }
+//     }
+//   },
+//   Tweets: {
+//     async user(tweet: Tweet) {
+//       try {
+//         const tweetAuthor = await admin
+//           .firestore()
+//           .doc(`users/${tweet.userId}`)
+//           .get();
+
+//         return tweetAuthor.data();
+//       } catch (error) {
+//         throw new ApolloError(error);
+//       }
+//     }
+//   },
+//   Query: {
+//     async tweets() {
+//       const tweets = await admin
+//         .firestore()
+//         .collection("tweets")
+//         .get();
+
+//       return tweets.docs.map((tweet) => tweet.data());
+//     },
+//     async user(_, args) {
+//       try {
+//         const userDoc = await admin
+//           .firestore()
+//           .doc(`users/${args.id}`)
+//           .get();
+
+//         const user = userDoc.data();
+
+//         return user || new ValidationError("User ID not found");
+//       } catch (error) {
+//         throw new ApolloError(error);
+//       }
+//     }
+//   }
+// };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,23 +1,23 @@
-import { gql } from "apollo-server-express";
+// import { gql } from "apollo-server-express";
 
-export const typeDefs = gql`
-  type User {
-    id: ID!
-    screenName: String!
-    statusesCount: Int!
-    tweets: [Tweets]!
-  }
+// export const typeDefs = gql`
+//   type User {
+//     id: ID!
+//     screenName: String!
+//     statusesCount: Int!
+//     tweets: [Tweets]!
+//   }
 
-  type Tweets {
-    id: ID!
-    text: String!
-    userId: String!
-    user: User!
-    likes: Int!
-  }
+//   type Tweets {
+//     id: ID!
+//     text: String!
+//     userId: String!
+//     user: User!
+//     likes: Int!
+//   }
 
-  type Query {
-    tweets: [Tweets]
-    user(id: String!): User
-  }
-`;
+//   type Query {
+//     tweets: [Tweets]
+//     user(id: String!): User
+//   }
+// `;
